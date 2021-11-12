b53e62aa051881eb0cf1cff9283b33d85602be3f
Milana.Anarbekova <milana.anarbekova78@gmail.com>
2021-11-12 17:05:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android1lesson10/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android1lesson10;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.textfield.TextInputEditText;
+
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private TextInputEditText userName,password;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
+        button= findViewById(R.id.button);
+        userName= findViewById(R.id.loginET);
+        password =  findViewById(R.id.passwordET);
+
+        String url = "https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
+
+        Glide.with(this).load(url).into(imageView);
+
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (userName.getText().toString().length() > 0) {
+                    button.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.color_black));
+
+                } else {
+                    button.setBackground(ContextCompat.getDrawable(MainActivity.this,R.drawable.color_grey));
+                }
+            }
+        });
+    }
+
+}
